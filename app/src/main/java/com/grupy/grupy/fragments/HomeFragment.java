2aa94cb5bf426cf9559b8da418fff755b8e52cee
Gerard <gerard.navarro22@gmail.com>
2020-07-15 12:09:55 +0200

    Implementation of alert dialogs and first implementation of navigation view

--- app/src/main/java/com/grupy/grupy/fragments/HomeFragment.java
+++ app/src/main/java/com/grupy/grupy/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.grupy.grupy.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +8,17 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.grupy.grupy.R;
+import com.grupy.grupy.activities.PostActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    View mView;
+    FloatingActionButton mPost;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,6 +28,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        mView = inflater.inflate(R.layout.fragment_home, container, false);
+        mPost = mView.findViewById(R.id.btnPost);
+        mPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToPost();
+            }
+        });
+        return mView;
+    }
+
+    private void goToPost() {
+        Intent intent = new Intent(getContext(), PostActivity.class);
+        startActivity(intent);
     }
 }
